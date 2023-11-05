44af9d6fe6af9c193e57d3d6fd81aec443ffa117
vehanmr <vehanrathnayake6@gmail.com>
2023-11-05 23:21:50 +0530

    python development

--- 14.functions.py
+++ 14.functions.py
@@ -13,24 +13,41 @@
 #             print(f'buzz({num})')
 #         else:
 #             print(num)
-#
-#
+
 # function1(u_input)
 
 # u_input = int(input("Enter an integer: "))
 
-num = int(input("Enter an integer: "))
+# num = int(input("Enter an integer: "))
+#
+# def function1(num):
+#     # for num in range(1, u_input):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print(f'fizzbuzz({num})')
+#     elif num % 3 == 0:
+#         print(f'fizz({num})')
+#     elif num % 5 == 0:
+#         print(f'buzz({num})')
+#     else:
+#         print(num)
+#
+#
+# function1(num)
 
-def function1(num):
-    # for num in range(1, u_input):
-    if num % 3 == 0 and num % 5 == 0:
-        print(f'fizzbuzz({num})')
-    elif num % 3 == 0:
-        print(f'fizz({num})')
-    elif num % 5 == 0:
-        print(f'buzz({num})')
-    else:
-        print(num)
+u_input = int(input("Enter an integer: "))
+import time
 
+def function1(u_input):
+    for num in range(1, u_input):
+        if num % 3 == 0 and num % 5 == 0:
+            print(f'fizzbuzz({num})')
+        elif num % 3 == 0:
+            print(f'fizz({num})')
+        elif num % 5 == 0:
+            print(f'buzz({num})')
+        else:
+            print(num)
 
-function1(num)
+print("hello")
+time.sleep(5)
+function1(u_input)
---
+++ 15.activity6.py
@@ -0,0 +1,8 @@
+user = input("Enter user name: ")
+
+
+def username(user):
+    print(f'Hello {user}')
+
+
+username(user)
